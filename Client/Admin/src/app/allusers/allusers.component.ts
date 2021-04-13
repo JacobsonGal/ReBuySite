7510ce2840a26a6524083bd350bbfc71deae476a
avivhorovitz <avivhorovitz1@gmail.com>
2021-04-13 11:14:02 +0300

    socket works(?)

--- Client/Admin/src/app/allusers/allusers.component.ts
+++ Client/Admin/src/app/allusers/allusers.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import  { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-allusers',
   templateUrl: './allusers.component.html',
-  styleUrls: ['./allusers.component.scss']
+  styleUrls: ['./allusers.component.scss'],
 })
 export class AllusersComponent implements OnInit {
+  allUsers: Number;
 
-  constructor() { }
+  constructor(private service: DatabaseService) {}
 
   ngOnInit(): void {
+    this.fetchUsers();
   }
-
+  fetchUsers(): void {
+    this.service.getAllUsers().subscribe((users) => {
+      this.allUsers = users.data.length;
+    });
+  }
 }

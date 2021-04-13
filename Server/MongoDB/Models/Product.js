d30db5b9a1faa3b2905a89e58cc2bc58e5807c54
JacobsonGal <jacobsongal@gmail.com>
2021-04-13 10:53:53 +0300

    add category

--- Server/MongoDB/Models/Product.js
+++ Server/MongoDB/Models/Product.js
@@ -7,6 +7,7 @@ const Product = new Schema(
     name: { type: String, required: true },
     condition: { type: String, required: true },
     description: { type: String, required: true },
+    category: { type: String, required: true },
     address: { type: String, required: true },
     price: { type: Number, required: true },
     owner: { type: Schema.Types.ObjectId, ref: "User" },

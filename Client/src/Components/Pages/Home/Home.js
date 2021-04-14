cc6c98deef8cd755147f463edb8eaa22a974c450
Amit Klein <AmitKlain1@gmail.com>
2021-04-14 13:52:05 +0300

    updated

--- Client/src/Components/Pages/Home/Home.js
+++ Client/src/Components/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Page from "../../Utils/Page";
 import ProductsList from "./Products";
 export default function Home({ title, setTitle, setActive }) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   return (
     <Page

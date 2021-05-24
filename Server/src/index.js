94010b6d12e5cb54bcbbdd2eedf464ffce99a5ac
JacobsonGal <jacobsongal@gmail.com>
2021-05-24 16:14:18 +0300

    Update index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
-app.use("./netlify/functions/api", productRouter);
-app.use("./netlify/functions/api", userRouter);
+app.use("/.netlify/functions/api", productRouter);
+app.use("/.netlify/functions/api", userRouter);
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {

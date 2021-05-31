c2cde2b50376f02e9d456c58642e5ad89a1da665
JacobsonGal <jacobsongal@gmail.com>
2021-05-31 15:48:38 +0300

    Welcome page

--- src/Components/SSO/Registration.js
+++ src/Components/SSO/Registration.js
@@ -4,16 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../../Assets/Images/ReBuyLogo.png";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
+import Welcome from "./Welcome";
 
 export default function Registration() {
   const [registered, setRegistered] = useState(false);
+  const [welcomeDone, setWelcomeDone] = useState(false);
   return (
     <div className="login">
       <div>
         <img src={logo} alt="logo" className="loginLogo" />
       </div>
       <div className="Form">
-        {registered ? (
+        {!welcomeDone ? (
+          <Welcome setWelcomeDone={setWelcomeDone} />
+        ) : registered ? (
           <LogIn setRegistered={setRegistered} />
         ) : (
           <SignUp setRegistered={setRegistered} />

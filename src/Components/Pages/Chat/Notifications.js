66a6fb723bf93c2b5aa4f524c55099f3483cc0dd
JacobsonGal <jacobsongal@gmail.com>
2021-06-07 17:17:12 +0300

    changed

--- src/Components/Pages/Chat/Notifications.js
+++ src/Components/Pages/Chat/Notifications.js
@@ -7,22 +7,32 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/analytics";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { AuthContext } from "../../SSO/Auth";
 import { Link, useParams } from "react-router-dom";
-
+import { Button } from "@material-ui/core";
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
 export default function Notifications({ title, setTitle, setActive }) {
-  const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);
+  // const { currentUser } = useContext(AuthContext);
+  // const user = currentUser;
   const [loading, setLoading] = useState(false);
   let tmp = [{ name: "loading" }];
+
   const [products, setProducts] = useState(tmp);
   const [sellerId, setSellerId] = useState();
   const [secondaryId, setScondaryId] = useState();
+  const [selling, setSelling] = useState(false);
+  const [itemId, setItemId] = useState("");
+
+  // console.log(currentUser);
+
   firestore
-    .collection(`products`).where("ownerId", "==", user?.uid)
+    .collection(`products`)
+    .where("ownerId", "==", currentUser?.uid)
     .get()
     .then((querySnapshot) => {
       let temp = [];
@@ -32,11 +42,8 @@ export default function Notifications({ title, setTitle, setActive }) {
     .catch((error) => {
       console.log("Error getting documents: ", error);
     });
+  // eslint-disable-next-line no-lone-blocks
   {
-
-
-
-
     return (
       <Page
         loading={loading}
@@ -47,51 +54,58 @@ export default function Notifications({ title, setTitle, setActive }) {
         FAB="none"
         dots={false}
       >
-        <div className="card contacts_card">
-          <div className="card-header">
-
-            <div className="col-12">
-              <div className="header ">
-                <h1>Choose Product</h1>
+        {!selling ? (
+          <div className="card contacts_card">
+            <div className="card-header">
+              <div className="col-12">
+                <div className="header ">
+                  <h1>Choose Product</h1>
+                </div>
               </div>
-
+            </div>
+            <div className="card-body contacts_body">
+              <ui className="contacts">
+                <li className="active">
+                  {products.map((item, index) => (
+                    <>
+                      <div className="d-flex bd-highlight">
+                        <div className="img_cont">
+                          <img
+                            src={
+                              item.photo ||
+                              "https://api.adorable.io/avatars/23/abott@adorable.png"
+                            }
+                            alt="userPhoto"
+                            className="rounded-circle user_img"
+                          />
+                        </div>
+                        <div className="user_info">
+                          <span>
+                            <Button
+                              onClick={() => {
+                                console.log(item);
+                                setItemId(item?.seconderyId);
+                                setSelling(true);
+                              }}
+                              key={index}
+                            >
+                              {item.name}
+                            </Button>
+                          </span>
+                          <p></p>
+                        </div>
+                      </div>
+                      <hr />
+                    </>
+                  ))}
+                </li>
+              </ui>
             </div>
           </div>
-          <div className="card-body contacts_body">
-            <ui className="contacts">
-              <li className="active">
-                {products.map((item, index) => (
-                  <>
-                    <div className="d-flex bd-highlight">
-                      <div className="img_cont">
-                        <img
-                          src={
-                            item.photo ||
-                            "https://api.adorable.io/avatars/23/abott@adorable.png"
-                          }
-                          className="rounded-circle user_img"
-                        />
-                      </div>
-                      <div className="user_info">
-                        <span >
-                          <Link
-                            // onClick=
-                            key={index}
-                          >
-                            {item.name}
-                          </Link>
-                        </span>
-                        <p></p>
-                      </div>
-                    </div>
-                    <hr />
-                  </>
-                ))}
-              </li>
-            </ui >
-          </div >
-        </div >
-      </Page >
+        ) : (
+          <SellerArea sellerId={currentUser?.uid} prodId={itemId} />
+        )}
+      </Page>
     );
   }
 }
@@ -115,12 +129,10 @@ function SellerArea({ sellerId, prodId }) {
     <>
       <div className="card contacts_card">
         <div className="card-header">
-
           <div className="col-12">
             <div className="header ">
               <h1>people who interested your product</h1>
             </div>
-
           </div>
         </div>
         <div className="card-body contacts_body">
